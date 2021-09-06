89cc7da4c8b13ce1baa44f04ddd36c29169579d8
majid3399769 <shaikmajid18@gmail.com>
2021-09-06 00:14:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-@app.route("/")
-def hello():
-    return "Welcome to Azure web application"
+app = dash.Dash()
+application = app.server
 
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-@app.route("/main")
-def main():
-    return "Main Page"
+    html.Div(children='''
+        This is Dash running on Azure App Service.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
